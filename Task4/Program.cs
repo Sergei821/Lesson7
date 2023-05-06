ec6ebfec2f5f43aff3e2f1e353b45be5aea6a41c
Sergei821 <ps16111982@gmail.com>
2023-05-07 02:54:42 +0300

    zad4

---
+++ Task4/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 3: Задайте двумерный массив. Найдите сумму элементов главной диагонали.
+int ReadInt(string message)
+{
+    System.Console.Write($"{message} > ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+int[,] Generate2DArray(int cntRows, int cntColumns)
+{
+    int[,] array = new int[cntRows, cntColumns];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = i + j;
+        }
+    }
+    return array;
+}
+
+void Print2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int SumDiagonal(int[,] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        sum += array[i, i];
+    }
+    return sum;
+}
+
+int rows = ReadInt("Введите количество строк");
+int columns = ReadInt("Введите количество колонок");
+int[,] arr = Generate2DArray(rows, columns);
+Print2DArray(arr);
